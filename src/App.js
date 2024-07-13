35e9518636e7b8564a49c707b523af9689052790
StefanPenkVivino <stefan.penk@vivino.com>
2024-07-13 19:59:12 +0200

    first layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="categories-container">
+      <div className="cateogry-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h2>Jackets</h2>
+          <h2>Shop Now</h2>
+        </div>
+      </div>
+      <div className="cateogry-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <h2>Shop Now</h2>
+        </div>
+      </div>
+      <div className="cateogry-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h2>Sneakers</h2>
+          <h2>Shop Now</h2>
+        </div>
+      </div>
+      <div className="cateogry-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h2>Womens</h2>
+          <h2>Shop Now</h2>
+        </div>
+      </div>
+      <div className="cateogry-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h2>Mens</h2>
+          <h2>Shop Now</h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
